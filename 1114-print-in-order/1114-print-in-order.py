1623b0b58ff21f4ec1f6190ddceef0085541387c
Shahina Shaik <115132561+Shahina-2810@users.noreply.github.com>
2024-08-22 12:14:23 +0530

    Time: 2911 ms (13.55%), Space: 17.2 MB (29.12%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.py
+++ 1114-print-in-order/1114-print-in-order.py
@@ -1,21 +1,15 @@
 class Foo:
-    def __init__(self):
-        
+    def __init__(self):     
         self.done = False
         self.second_done = False
 
-
-
     def first(self, printFirst: 'Callable[[], None]') -> None:
         
-        # printFirst() outputs "first". Do not change or remove this line.
-        
+        # printFirst() outputs "first". Do not change or remove this line.  
         printFirst()
         self.done=True
 
-
-    def second(self, printSecond: 'Callable[[], None]') -> None:
-        
+    def second(self, printSecond: 'Callable[[], None]') -> None:   
         # printSecond() outputs "second". Do not change or remove this line.
         while not self.done:
             continue
@@ -23,11 +17,8 @@ class Foo:
         printSecond()
         self.second_done=True
 
-
-    def third(self, printThird: 'Callable[[], None]') -> None:
-        
+    def third(self, printThird: 'Callable[[], None]') -> None:     
         # printThird() outputs "third". Do not change or remove this line.
         while not self.second_done:
             continue
         printThird()
-        self.second_done=True
