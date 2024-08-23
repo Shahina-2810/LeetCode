9b0a7e5a2069d3eb795a87e5239f8a73b16be018
Shahina Shaik <115132561+Shahina-2810@users.noreply.github.com>
2024-08-23 12:51:06 +0530

    Time: 11 ms (32.20%), Space: 41.4 MB (13.01%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+ 
+    private static boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+                            if (solve(board)) {
+                                return true;
+                            }
+                            board[i][j] = '.'; // Backtrack
+                        }
+                    }
+                    return false; // No valid number found
+                }
+            }
+        }
+        return true;
+    }
+        private static boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c || board[i][col] == c || board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
