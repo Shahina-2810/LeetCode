a2f036b31f2f34dd51b1cb187184ab1346ae731f
Shahina Shaik <115132561+Shahina-2810@users.noreply.github.com>
2024-08-27 17:04:45 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (54.50%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,17 +1,35 @@
-class Solution:
-    def getPermutation(self, n: int, k: int) -> str:
-        factorial = [1] * n
-        for i in range(1, n):
-            factorial[i] = factorial[i-1] * i
+class Solution {
+    private static int[] fact = {0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
     
-        k -= 1
-        nums = list(range(1, n + 1))
-        permutation = []
+    private String getPermutation(int n, int k, boolean[] nums, char[] str, int index) {
+        int i = 0, m = nums.length;
+        if(n == 1) {
+            while(i < m && nums[i]) ++i;
+            str[index++]=(char)('0'+i+1);
+            return String.valueOf(str);
+        }
+        if(k == 0) {
+            while(i < m) {
+                if(!nums[i]) str[index++]=(char)('0'+i+1);
+                ++i;
+            }
+            return String.valueOf(str);
+        }
+        
+        int div = k/fact[n-1], mod = k%fact[n-1], j = -1;
+        while(i < m-1 && div != j) {
+            if(!nums[i]) ++j;
+            if(j == div) break;
+            ++i;
+        }
+        str[index++]=(char)('0'+i+1);
+        if(i < m) nums[i]=true;
+        return getPermutation(n-1, mod, nums, str, index); 
+    }
 
-        for i in range(n, 0, -1):
-            index = k // factorial[i - 1]
-            k %= factorial[i - 1]
-            permutation.append(nums[index])
-            nums.pop(index)
-    
-        return ''.join(map(str, permutation))
+    public String getPermutation(int n, int k) {
+        boolean[] nums = new boolean[n];
+        char[] charArr = new char[n];
+        return getPermutation(n, k-1, nums, charArr, 0);  
+    }
+}
