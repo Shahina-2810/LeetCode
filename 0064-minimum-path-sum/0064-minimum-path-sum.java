05538c54919aada32a7c741f90e753d98ae78dd4
Shahina Shaik <115132561+Shahina-2810@users.noreply.github.com>
2024-08-27 16:48:11 +0530

    Time: 5 ms (12.16%), Space: 47 MB (5.25%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,31 +1,23 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-       if (grid == null || grid.length == 0) return 0;
-        
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] dp = new int[m][n];
-        
-        // Initialize the top-left cell
-        dp[0][0] = grid[0][0];
-        
-        // Initialize the first row
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = dp[0][j-1] + grid[0][j];
-        }
-        
-        // Initialize the first column
-        for (int i = 1; i < m; i++) {
-            dp[i][0] = dp[i-1][0] + grid[i][0];
-        }
-        
-        // Fill in the rest of the dp table
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+      
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int [] [] dp = new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=grid[i][j];
+                    continue;
+                }
+                int left= (int)(1e9);
+                int up= (int)(1e9);
+                if(j>0) left = grid[i][j]+dp[i][j-1];
+                if(i>0) up = grid[i][j]+dp[i-1][j];
+                dp[i][j] = Math.min(left,up);
             }
         }
-        
-        return dp[m-1][n-1]; 
+        return dp[n-1][m-1]; 
     }
 }
