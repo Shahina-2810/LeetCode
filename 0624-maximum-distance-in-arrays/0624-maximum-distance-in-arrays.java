08f1f9863e0525454b9622de3446cb0831913f94
Shahina Shaik <115132561+Shahina-2810@users.noreply.github.com>
2024-08-31 09:56:48 +0530

    Time: 5 ms (99.38%), Space: 64.3 MB (67.60%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -1,16 +1,20 @@
 class Solution {
     public int maxDistance(List<List<Integer>> arrays) {
-          int smallest = arrays.get(0).get(0);
-        int biggest = arrays.get(0).get(arrays.get(0).size() - 1);
-        int maxDistance = 0;
+         int globalMin = arrays.get(0).get(0);
+        int globalMax = arrays.get(0).get(arrays.get(0).size() - 1);
+        int result = 0;
 
         for (int i = 1; i < arrays.size(); i++) {
-            maxDistance = Math.max(maxDistance, Math.abs(arrays.get(i).get(arrays.get(i).size() - 1) - smallest));
-            maxDistance = Math.max(maxDistance, Math.abs(biggest - arrays.get(i).get(0)));
-            smallest = Math.min(smallest, arrays.get(i).get(0));
-            biggest = Math.max(biggest, arrays.get(i).get(arrays.get(i).size() - 1));
+            List<Integer> curr = arrays.get(i);
+            int localMin = curr.get(0);
+            int localMax = curr.get(curr.size() - 1);
+
+            result = Math.max(result, Math.max(localMax - globalMin, globalMax - localMin));
+
+            globalMin = Math.min(globalMin, localMin);
+            globalMax = Math.max(globalMax, localMax);
         }
 
-        return maxDistance;
+        return result;
     }
 }
