152eeaaac5fdc50ed015f40f00cad4be6c134750
Shahina Shaik <115132561+Shahina-2810@users.noreply.github.com>
2024-09-29 23:06:22 +0530

    Time: 57 ms (33.73%), Space: 60.3 MB (72.52%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.java
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.java
@@ -1,81 +1,119 @@
-import java.util.HashMap;
+public class Node {
+
+    int freq;
+    Node prev;
+    Node next;
+    Set<String> keys = new HashSet<>();
+
+    Node(int freq) {
+        this.freq = freq;
+    }
+}
 
 class AllOne {
 
-    private HashMap<String, Integer> d;
-    private int op;
-    private String res;
+    Node head;
+    Node tail;
+    Map<String, Node> map = new HashMap<>();
 
-    public AllOne() {
-        d = new HashMap<>();
-        op = 0;
-        res = "";
+    AllOne() {
+        head = new Node(0);
+        tail = new Node(0);
+        head.next = tail;
+        tail.prev = head;
     }
 
     public void inc(String key) {
-        op = 0;
-        d.put(key, d.getOrDefault(key, 0) + 1);
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            int freq = node.freq;
+            node.keys.remove(key);
+
+            Node nextNode = node.next;
+            if (nextNode == tail || nextNode.freq != freq + 1) {
+                Node newNode = new Node(freq + 1);
+                newNode.keys.add(key);
+                newNode.prev = node;
+                newNode.next = nextNode;
+                node.next = newNode;
+                nextNode.prev = newNode;
+                map.put(key, newNode);
+            } else {
+                nextNode.keys.add(key);
+                map.put(key, nextNode);
+            }
+
+            if (node.keys.isEmpty()) {
+                removeNode(node);
+            }
+        } else {
+            Node firstNode = head.next;
+            if (firstNode == tail || firstNode.freq > 1) {
+                Node newNode = new Node(1);
+                newNode.keys.add(key);
+                newNode.prev = head;
+                newNode.next = firstNode;
+                head.next = newNode;
+                firstNode.prev = newNode;
+                map.put(key, newNode);
+            } else {
+                firstNode.keys.add(key);
+                map.put(key, firstNode);
+            }
+        }
     }
 
     public void dec(String key) {
-        op = 0;
-        if (d.containsKey(key)) {
-            if (d.get(key) > 1) {
-                d.put(key, d.get(key) - 1);
+        if (!map.containsKey(key)) {
+            return;
+        }
+
+        Node node = map.get(key);
+        node.keys.remove(key);
+        int freq = node.freq;
+
+        if (freq == 1) {
+            map.remove(key);
+        } else {
+            Node prevNode = node.prev;
+            if (prevNode == head || prevNode.freq != freq - 1) {
+                Node newNode = new Node(freq - 1);
+                newNode.keys.add(key);
+                newNode.prev = prevNode;
+                newNode.next = node;
+                prevNode.next = newNode;
+                node.prev = newNode;
+                map.put(key, newNode);
             } else {
-                d.remove(key);
+                prevNode.keys.add(key);
+                map.put(key, prevNode);
             }
+        }
+
+        if (node.keys.isEmpty()) {
+            removeNode(node);
         }
     }
 
     public String getMaxKey() {
-        if (op == 1) {
-            return res;
+        if (tail.prev == head) {
+            return "";
         }
-        op = 1;
-        if (!d.isEmpty()) {
-            int a = (int) d.values().toArray()[0];
-            String b = (String) d.keySet().toArray()[0];
-            for (String key : d.keySet()) {
-                if (d.get(key) > a) {
-                    a = d.get(key);
-                    b = key;
-                }
-            }
-            res = b;
-            return b;
-        }
-        res = "";
-        return "";
+        return tail.prev.keys.iterator().next();
     }
 
     public String getMinKey() {
-        if (op == 2) {
-            return res;
+        if (head.next == tail) {
+            return "";
         }
-        op = 2;
-        if (!d.isEmpty()) {
-            int a = (int) d.values().toArray()[0];
-            String b = (String) d.keySet().toArray()[0];
-            for (String key : d.keySet()) {
-                if (d.get(key) < a) {
-                    a = d.get(key);
-                    b = key;
-                }
-            }
-            res = b;
-            return b;
-        }
-        res = "";
-        return "";
+        return head.next.keys.iterator().next();
+    }
+
+    private void removeNode(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
     }
 }
-
-/**
- * Your AllOne object will be instantiated and called as such:
- * AllOne obj = new AllOne();
- * obj.inc(key);
- * obj.dec(key);
- * String param_3 = obj.getMaxKey();
- * String param_4 = obj.getMinKey();
- */
