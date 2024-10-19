c1620e44d397ee6ca521c6c94e702985b3c6ebbd
Shahina Shaik <115132561+Shahina-2810@users.noreply.github.com>
2024-10-19 20:54:30 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (73.93%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
@@ -1,11 +1,8 @@
 class Solution {
     public char findKthBit(int n, int k) {
         if(n==1) return '0';
-        if(k<(1<<n)/2)
-            return findKthBit(n-1,k);
-        if(k==(1<<n)/2)
-            return '1';
-        else
-            return (findKthBit(n-1,(1<<n)-k) == '0') ? '1' : '0';
+        if(k<(1<<n)/2) return findKthBit(n-1,k);
+        if(k==(1<<n)/2) return '1';
+        else return (findKthBit(n-1,(1<<n)-k) == '0') ? '1' : '0';
     }
 }
